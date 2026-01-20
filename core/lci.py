86e16027fb389f8ef1ed90abcf6aeb512c809dc1
Marin Pellan <pellan.marin@gmail.com>
2026-01-20 11:14:45 +0100

    Changes to material demand calculations + first LCA results

--- core/lci.py
+++ core/lci.py
@@ -1,30 +1,41 @@
 # ======================================================
-# Ecoinvent
+# Chosen LCIs
 # ======================================================
-INVENTORIES_CA_ei = {
-    "Cu concentrate from Au mine": ("gold-silver mine operation and beneficiation", "copper concentrate, sulfide ore", 'CA-QC'),
-    "Cu concentrate from Cu mine": ("copper mine operation and beneficiation, sulfide ore", "copper concentrate, sulfide ore", 'CA'),
-    "Au-Ag ingot": ("gold-silver mine operation and beneficiation", "gold-silver, ingot", "CA-QC"),
-    "Pb concentrate": ("gold-silver mine operation and beneficiation", "lead concentrate", "CA-QC"),
-    "Zn concentrate": ("gold-silver mine operation and beneficiation", "zinc concentrate", "CA-QC"),
-    "Au refined": ("gold-silver mine operation with refinery", "gold", "CA-QC"),
-    "Ag refined": ("gold-silver mine operation with refinery", "silver", "CA-QC"),
-    "Fe concentrate": ("iron ore mine operation and beneficiation", "iron ore concentrate", "CA-QC"),
-    "Ni concentrate": ("nickel mine operation and benefication to nickel concentrate, 16% Ni", "nickel concentrate, 16% Ni", "CA-QC"), #typo in EI
-}
+INVENTORIES = {
+    # Mining
+    ## CA
+    "Cu mining CA": ("copper mine operation and beneficiation, sulfide ore", "copper concentrate, sulfide ore", 'CA'),
+    "Ni mining CA": ("nickel mine operation and benefication to nickel concentrate, 16% Ni", "nickel concentrate, 16% Ni", "CA-QC"),
+    #"Li mining CA": (),
+    "Co mining CA": ("copper-cobalt mining, industrial, economic allocation", "copper-cobalt ore", 'CA'),
+    "C mining CA": ("graphite ore mining", "graphite ore, mined", 'CA'),
+    #"Nd mining CA": (),
 
 
-# ======================================================
-# Regioinvent
-# ======================================================
-INVENTORIES_CA_reg = {
-    "Cu concentrate from Au mine": ("gold-silver mine operation and beneficiation", "copper concentrate, sulfide ore", 'CA'),
-    "Cu concentrate from Cu mine": ("copper mine operation and beneficiation, sulfide ore", "copper concentrate, sulfide ore", 'CA'),
-    "Au-Ag ingot": ("gold-silver mine operation and beneficiation", "gold-silver, ingot", "CA"),
-    "Pb concentrate": ("gold-silver mine operation and beneficiation", "lead concentrate", "CA"),
-    "Zn concentrate": ("gold-silver mine operation and beneficiation", "zinc concentrate", "CA"),
-    "Au refined": ("gold-silver mine operation with refinery", "gold", "CA"),
-    "Ag refined": ("gold-silver mine operation with refinery", "silver", "CA"),
-    "Fe concentrate": ("iron ore mine operation and beneficiation", "iron ore concentrate", "CA"),
-    "Ni concentrate": ("nickel mine operation and benefication to nickel concentrate, 16% Ni", "nickel concentrate, 16% Ni", "CA"), #typo in EI
+    ## World
+    "Cu mining World": ("market for copper concentrate, sulfide ore", "copper concentrate, sulfide ore", 'World'),
+    "Ni mining World": ("market for nickel concentrate, 16% Ni", "nickel concentrate, 16% Ni", 'World'),
+    #"Li mining World": (),
+    "Co mining World": ("market for copper-cobalt ore", "copper-cobalt ore", 'World'),
+    "C mining World": ("market for graphite ore, mined", "graphite ore, mined", 'World'),
+    #"Nd mining World": (),
+
+
+    # Refining
+    ## CA
+    "Cu refining CA": ("copper production, cathode, solvent extraction and electrowinning process", "copper, cathode", 'CA'),
+    "Ni refining CA": ("processing of nickel-rich materials", "nickel, class 1", 'CA'),
+    "Li refining CA": ("lithium carbonate production, from spodumene", "lithium carbonate", 'CA'),
+    "Co refining CA": ("cobalt sulfate production, from copper mining, economic allocation", "cobalt sulfate", 'CA'),
+    "C refining CA": ("graphite production", "graphite", "CA"),
+    #"Nd refining CA": (),
+
+    ## World
+    "Cu refining World": ("market for copper, cathode", "copper, cathode", 'World'),
+    "Ni refining World": ("market for nickel, class 1", "nickel, class 1", 'World'),
+    "Li refining World": ("lithium (Li) global market", "li commodities", 'GLO'),
+    "Co refining World": ("market for cobalt sulfate", "cobalt sulfate", 'World'),
+    "C refining World": ("market for graphite", "graphite", "World"),
+    "Nd refining World": ("neodymium (Nd) global market", "nd oxide (Nd2O3)", 'GLO'),
+
 }
