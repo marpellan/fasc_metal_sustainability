53f77f8326aa1328e67221b8a757ab32b816924b
Marin Pellan <pellan.marin@gmail.com>
2026-09-06 22:17:44 +0200

    Update new LCIs for sub

--- core/lci.py
+++ core/lci.py
@@ -6,37 +6,32 @@ INVENTORIES = {
     ## CA
     "Cu mining CA": ("copper mine operation and beneficiation, sulfide ore", "copper concentrate, sulfide ore", 'CA'),
     "Ni mining CA": ("nickel mine operation and benefication to nickel concentrate, 16% Ni", "nickel concentrate, 16% Ni", "CA-QC"),
-    #"Li mining CA": (),
-    "Co mining CA": ("copper-cobalt mining, industrial, economic allocation", "copper-cobalt ore", 'CA'),
-    "C mining CA": ("graphite ore mining", "graphite ore, mined", 'CA'),
-    #"Nd mining CA": (),
+    "Li mining CA": ("spodumene production", "spodumene", 'CA'),
+    "Co mining CA": ("cobalt hydroxide, via hydrometallurigcal ore procesing, economic allocation", "cobalt hydroxide", 'CA'),
+    #"C mining CA": ("graphite ore mining", "graphite ore, mined", 'CA'),
 
 
     ## World
     "Cu mining World": ("market for copper concentrate, sulfide ore", "copper concentrate, sulfide ore", 'World'),
     "Ni mining World": ("market for nickel concentrate, 16% Ni", "nickel concentrate, 16% Ni", 'World'),
-    #"Li mining World": (),
-    "Co mining World": ("market for copper-cobalt ore", "copper-cobalt ore", 'World'),
-    "C mining World": ("market for graphite ore, mined", "graphite ore, mined", 'World'),
-    #"Nd mining World": (),
+    "Li mining World": ("market for spodumene", "spodumene", 'World'),
+    "Co mining World": ("market for cobalt hydroxide", "cobalt hydroxide", 'World'),
+    "C mining World": ("natural graphite concentration", "graphite ore, concentrated", 'CN'),
 
 
     # Refining or Mining+refining
     ## CA
-    "Cu mining+refining CA": ("copper production, cathode, solvent extraction and electrowinning process", "copper, cathode", 'CA'),
+    "Cu refining CA": ("copper production, cathode, solvent extraction and electrowinning process", "copper, cathode", 'CA'),
     "Ni refining CA": ("processing of nickel-rich materials", "nickel, class 1", 'CA'),
-    "Li refining CA": ("lithium carbonate production, from spodumene", "lithium carbonate", 'CA'),
+    "Li refining CA": ("lithium hydroxide production, battery grade", "lithium hydroxide, battery grade", 'CA'),
     "Co refining CA": ("cobalt sulfate production, from copper mining, economic allocation", "cobalt sulfate", 'CA'),
-    "C refining CA": ("synthetic graphite production, battery grade", "synthetic graphite, battery grade", 'CA'),
-    "Nd refining CA": ("rare earth oxides production, from rare earth carbonate concentrate", "neodymium oxide", 'CA'),
+    "C refining CA": ("graphite production", "graphite", 'CA'),
 
     ## World
-    "Cu mining+refining World": ("copper production, cathode, solvent extraction and electrowinning process", "copper, cathode", 'World'),
-    "Ni refining World": ("processing of nickel-rich materials", "nickel, class 1", 'GLO'),
-    "Li refining World": ("lithium (Li) global market", "li commodities", 'GLO'),
+    "Cu mining+refining World": ("market for copper, cathode", "copper, cathode", 'World'),
+    "Ni refining World": ("market for nickel, class 1", "nickel, class 1", 'World'),
+    "Li refining World": ("market for lithium hydroxide, battery grade", "lithium hydroxide, battery grade", 'World'),
     "Co refining World": ("market for cobalt sulfate", "cobalt sulfate", 'World'),
-    "C refining World": ("synthetic graphite production, battery grade", "synthetic graphite, battery grade", 'CN'),
-    "Nd refining World": ("rare earth oxides production, from rare earth carbonate concentrate", "neodymium oxide", 'CN'),
-    #"Nd refining World": ("neodymium (Nd) global market", "nd oxide (Nd2O3)", 'GLO'),
+    "C refining World": ("graphite production", "graphite", 'CA'),
 
 }
